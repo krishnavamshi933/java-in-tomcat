6e0e39c3b22d58768b98e295099642066c5c779b
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2022-07-26 18:02:24 +0530

    ff

--- vars/git_checkout.groovy
+++ vars/git_checkout.groovy
@@ -1,7 +1,7 @@
 def deploy_tomcat() {
     
     echo "git checkout."
-    
+    git branch: 'main', credentialsId: 'krishnavamshi933', url: 'git@github.com:krishnavamshi933/java-in-tomcat.git'
 
 }
 
