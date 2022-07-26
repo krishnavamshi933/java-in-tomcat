a095fc9734facd9051399bea13d86dc7648ac6a5
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2022-07-26 18:10:34 +0530

    h

--- vars/git_checkout.groovy
+++ vars/git_checkout.groovy
@@ -1,7 +1,7 @@
 def git_checkout() {
     
     echo "git checkout."
-    git branch: 'main', credentialsId: 'krishnavamshi933', url: 'git@github.com:krishnavamshi933/java-in-tomcat.git'
+    git branch: 'main', credentialsId: 'krishnavamshi933', url: 'git@github.com:krishnavamshi933/sparkjava-war-example.git'
 
 }
 
