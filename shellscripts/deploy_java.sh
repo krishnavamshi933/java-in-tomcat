5ce6ff9719f5863253c8e3f240b46ea31071f658
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2022-07-29 17:25:42 +0530

    bb

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
-aws s3 cp s3://krishnavamshi/java-project/java_code/sparkjava-hello-world-1.0.war .
+aws s3 cp s3://krishnavamshi/java_code/sparkjava-hello-world-1.0.war .
 scp sparkjava-hello-world-1.0.war ec2-user@172.31.7.231:/home/ec2-user/tomcat/apache-tomcat-9.0.65/webapps
 ssh ec2-user@172.31.7.231 << HERE
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.65/bin/
